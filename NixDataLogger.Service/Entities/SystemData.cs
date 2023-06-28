2705dba63afd905dde21e576a9e6f5c19ca07ee0
Matheus Soares <69659304+msoareseq@users.noreply.github.com>
2023-06-28 18:14:29 -0300

    Update SystemData.cs

--- NixDataLogger.Service/Entities/SystemData.cs
+++ NixDataLogger.Service/Entities/SystemData.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NixDataLogger.Service
+namespace NixDataLogger.Service.Entities
 {
     public class SystemData
     {
