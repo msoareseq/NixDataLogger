73eb2d6e991d2793172a54a408412c8878c043c4
Matheus Soares <69659304+msoareseq@users.noreply.github.com>
2023-06-29 13:54:50 -0300

    Update SimClient.cs
    Created a more elaborated SimClient with different Data Types

--- NixDataLogger.Service/Clients/SimClient.cs
+++ NixDataLogger.Service/Clients/SimClient.cs
@@ -9,6 +9,7 @@ namespace NixDataLogger.Service.Clients
 {
     internal class SimClient : IApiClient
     {
+        private readonly string[] stringValues = new string[] { "Approved", "Rejected", "Undefined", "Offline", "Online", "Empty", "Full", "Ok", "Fail" };
         public IEnumerable<TagData> GetTagData(IEnumerable<Tag> tags)
         {
             foreach (var tag in tags)
@@ -16,10 +17,44 @@ namespace NixDataLogger.Service.Clients
                 yield return new TagData()
                 {
                     TagName = tag.TagName,
-                    Value = new Random().NextDouble().ToString(),
+                    Value = GenerateRandomValue(tag),
                     Timestamp = DateTime.Now,
                     QualityCode = 1,                     
                 };
+            }
+        }
+
+        private object GenerateRandomValue(Tag tag)
+        {
+            Random random = new Random();
+            if (!tag.IsNumeric)
+            {
+                if (tag.DataType == Tag.TagType.Boolean)
+                {
+                    if(random.Next(0, 2) == 0)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    return stringValues[random.Next(0, stringValues.Length)];
+                }
+            }
+            else
+            {
+                if (tag.DataType == Tag.TagType.Integer)
+                {
+                    return random.Next(0, 100);
+                }
+                else
+                {
+                    return random.NextDouble() * 100;
+                }
             }
         }
     }
