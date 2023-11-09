222c2edacca20f89913eeb3b712dc4a361c13ed5
Matheus Soares <69659304+msoareseq@users.noreply.github.com>
2023-11-09 00:04:30 -0300

    Update Program.cs

--- NixDataLogger.Service/Program.cs
+++ NixDataLogger.Service/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Hosting.WindowsServices;
 using Serilog;
 using Serilog.Sinks.SystemConsole.Themes;
 
@@ -7,7 +8,20 @@ namespace NixDataLogger.Service
     {
         public static void Main(string[] args)
         {
-            string configFileName = "nix.json";
+            string? baseDir = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            string defaultDir = @"C:\Nix\";
+
+            if (WindowsServiceHelpers.IsWindowsService())
+            {
+                Log.Information("Running as a Windows Service");
+            }
+            else
+            {
+                Log.Information("Running as a Console Application");
+            }
+
+
+            string configFileName = Path.Combine(baseDir ?? defaultDir, "nix.json");
             ServiceConfiguration serviceConfig;
 
 
@@ -15,14 +29,17 @@ namespace NixDataLogger.Service
 
             
             Log.Logger = new LoggerConfiguration()
-                .WriteTo.File("logs\\log.txt", rollingInterval: RollingInterval.Day)
+                .WriteTo.File(Path.Combine(baseDir ?? defaultDir, "logs\\log.txt"), rollingInterval: RollingInterval.Day)
                 .WriteTo.Console(theme: AnsiConsoleTheme.Code)
                 .CreateLogger();
+
             
+
             try
             {
                 Log.Information("Starting Nix...");
-                
+                Log.Information("Base Directory: {0}", baseDir);
+
                 if (!File.Exists(configFileName))
                 {
                     Log.Warning("Configuration file (nix.json) not found. Using default configuration file...");
